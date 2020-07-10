9527ef9da04ebd9b9db5768e893e3c9a6aee0c38
Khizar Kazmi <mashter.gee@gmail.com>
2020-07-10 21:53:14 +0500

    functionhi

---
+++ src/hi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Hi()
+{
+    return
+    <div>
+        this is first function my first function name hi
+
+    </div>;
+}
+ReactDOM.render(<Hi/>,document.querySelector('#root'))
